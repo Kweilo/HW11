edc8d9f71f48d1d400820a8db050517978cc5ff6
Kweilo <kweilo.yandere@mail.ru>
2024-11-28 01:18:08 +0300

    meow

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,52 @@
 public class Main {
+    //Задание 1
+    public static void findYear(int year) {
+        if (year < 1584) {
+            System.out.println("Год не может быть меньше 1585");
+        } else if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
+    }
+    //Задание 2
+    public static void findOS(int clientOS, int clientDeviceYear) {
+        if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+            if (clientDeviceYear <= 2015) {
+                System.out.println("Установите облегченную версию");
+            }
+        } else {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+            if (clientDeviceYear <= 2015) {
+                System.out.println("Установите облегченную версию");
+            }
+        }
+    }
+    //Задание 3
+    public static void findDistance(int deliveryDistance) {
+        if (deliveryDistance < 20) {
+            System.out.println("Потребуется дней: 1");
+        } else if (deliveryDistance >= 20 && deliveryDistance <= 60) {
+            System.out.println("Потребуется дней: 2");
+        } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
+            System.out.println("Потребуется дней: 3");
+        } else {
+            System.out.println("Доставки нет");
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Задание 1
+        int years = 2012;
+        findYear(years);
+
+        //Задание 2
+        int clientDeviceYear = 2010;
+        int clientOS = 0;
+        findOS(clientOS,clientDeviceYear);
+
+        //Задания 3
+        int deliveryDistance = 95;
+        findDistance(deliveryDistance);
     }
 }
