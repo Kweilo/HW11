bc4d1ad3f7294d6e9f03dc5f74bb086b4f65760f
Kweilo <kweilo.yandere@mail.ru>
2024-11-29 22:50:48 +0300

    meow

--- src/Main.java
+++ src/Main.java
@@ -11,16 +11,14 @@ public class Main {
     }
     //Задание 2
     public static void findOS(int clientOS, int clientDeviceYear) {
-        if (clientOS == 1) {
+        if (clientOS == 1 && clientDeviceYear >= 2015) {
             System.out.println("Установите версию приложения для Android по ссылке");
-            if (clientDeviceYear <= 2015) {
-                System.out.println("Установите облегченную версию");
-            }
-        } else {
+        } else if (clientOS == 1 && clientDeviceYear <= 2015) {
+            System.out.println("Установите облегченную версию");
+        } else if (clientOS == 0 && clientDeviceYear >= 2015) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-            if (clientDeviceYear <= 2015) {
-                System.out.println("Установите облегченную версию");
-            }
+        } else {
+            System.out.println("Установите облегченную версию");
         }
     }
     //Задание 3
